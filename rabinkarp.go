762ca67ccac6a96356a4ec73d31cb10233801752
Mustafa Altun <gmail@mustafaaltun.com>
2015-10-13 21:52:03 +0300

    Add initial implementation of SearchReader

--- rabinkarp.go
+++ rabinkarp.go
@@ -8,12 +8,97 @@ Provides searching multiple patterns with an average O(n+m) time complexity
 */
 package rabinkarp
 
+import "io"
+
 const base = 16777619
 
 // Search searches given patterns in txt and returns the matched ones. Returns
 // empty string slice if there is no match.
 func Search(txt string, patterns []string) []string {
 	in := indices(txt, patterns)
+	matches := make([]string, len(in))
+	i := 0
+	for j, p := range patterns {
+		if _, ok := in[j]; ok {
+			matches[i] = p
+			i++
+		}
+	}
+
+	return matches
+}
+
+// SearchReader searches given patterns in r(io.Reader) without buffering the
+// whole stream.
+func SearchReader(r io.Reader, patterns []string) []string {
+	min, max := minLen(patterns), maxLen(patterns)
+	chunksize := 4096
+
+	if chunksize < 2*max {
+		chunksize = 2 * max
+	}
+
+	txt := make([]byte, chunksize)
+	eof := false
+	round := 0
+	matches := make(map[int]int)
+
+	if n, err := io.ReadFull(r, txt); err != nil {
+		eof = true
+		txt = txt[:n]
+	}
+
+	hp := hashPatterns(patterns, min)
+	h := hash(string(txt[:min]))
+
+	var mult uint32 = 1 // mult = base^(m-1)
+	for i := 0; i < min-1; i++ {
+		mult = (mult * base)
+	}
+
+	var cp uint32 // prev char
+
+	for i := 0; i < len(txt); i++ {
+		//rem := len(txt) - i - 1
+		if !eof && len(txt)-i-1 < max {
+			buf2 := make([]byte, chunksize)
+			n, err := io.ReadFull(r, buf2)
+			if err != nil {
+				eof = true
+				buf2 = buf2[:n]
+			}
+
+			txt = append(txt[i:], buf2...)
+			i = 0
+			round++
+		}
+
+		if eof && len(txt)-i-1 < min {
+			break
+		}
+
+		if (round > 0) || (round == 0 && i > 0) {
+			h = h - mult*cp
+			h = h*base + uint32(txt[i+min-1])
+		}
+
+		if mps, ok := hp[h]; ok {
+			for _, pi := range mps {
+				pat := patterns[pi]
+				e := i + len(pat)
+				if _, ok := matches[pi]; !ok && e <= len(txt) && pat == string(txt[i:e]) {
+					matches[pi] = (round * max) + i
+				}
+			}
+		}
+
+		cp = uint32(txt[i])
+	}
+
+	return matchesFromIndices(matches, patterns)
+}
+
+func matchesFromIndices(in map[int]int, patterns []string) []string {
 	matches := make([]string, len(in))
 	i := 0
 	for j, p := range patterns {
@@ -91,6 +176,17 @@ func minLen(patterns []string) int {
 	m := len(patterns[0])
 	for i := range patterns {
 		if m > len(patterns[i]) {
+			m = len(patterns[i])
+		}
+	}
+
+	return m
+}
+
+func maxLen(patterns []string) int {
+	m := 0
+	for i := range patterns {
+		if m < len(patterns[i]) {
 			m = len(patterns[i])
 		}
 	}
